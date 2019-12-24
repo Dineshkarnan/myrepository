f7682846d84dd0ea0839b8361a8c92f26efce67d
Dinesh <dineshkarnanithi7@gmail.com>
2019-12-24 11:36:12 +0530

    commited

---
+++ Day1_java/src/obj/cts/org/Obj.java
@@ -0,0 +1,18 @@
+package obj.cts.org;
+
+public class Obj {
+	public void javacourse()
+		{
+	System.out.println("Java Course Started");
+		}
+
+	public void oraclecourse()
+	{
+		System.out.println("oracle course started");
+	}
+	public static void main(String[] args) {
+		Obj cs = new Obj();
+		cs.javacourse();
+		cs.oraclecourse();
+	}
+}
